0104ccd99f8e838627453f4aa36914ecfdb1a490
t19960804 <t19960804@gmail.com>
2022-10-20 20:58:43 +0800

    Initial Commit

---
+++ JKO_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JKO_Project
+//
+//  Created by t19960804 on 10/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
